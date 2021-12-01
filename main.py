b131ef30f4a76cfd4faa726fc49c126f2f2e3143
glt005 <glt005@bucknell.edu>
2021-12-01 17:07:57 -0500

    cleaned up code and added function to print tree

--- main.py
+++ main.py
@@ -5,45 +5,52 @@ START_NODE = 1
 END_NODE = 2
 LENGTH = 3
 
+class Graph:
 
-def stringToInt(edge):
-    for i in range(len(edge) - 1):
-        edge[i] = int(edge[i])
-    edge[3] = float(edge[3])
-    return edge
+    def stringToInt(self, edge):
+        for i in range(len(edge) - 1):
+            edge[i] = int(edge[i])
+        edge[3] = float(edge[3])
+        return edge
 
-def main():
-    edges = []
+    def printGraph(self, graph):
+        print("Number of vertices: " + str(self.vert_count) + "\n")
+        print("________________________Format________________________")
+        print("| START NODE  |" + " ------> " + "| [EDGE ID, END NODE, LENGTH] |")
+        for key in range(len(graph)):
+            print("| " + str(graph.keys()[key]) + " |" + " ------> " + "| " + str(graph.values()[key]) +  " |")
 
-    with open('small_test.txt') as f:
-        for line in f:
-            edges.append(line.split(" "))
+    def createGraph(self):
+        edges = []
 
-    vert_count = 0
-    for edge in edges:
-        stringToInt(edge)
-        if edge[START_NODE] >= vert_count or edge[END_NODE] >= vert_count:
-            if edge[START_NODE] > edge[END_NODE]:
-                vert_count = edge[START_NODE]
-            else:
-                vert_count = edge[END_NODE]
+        with open('small_test.txt') as f:
+            for line in f:
+                edges.append(line.split(" "))
+
+        self.vert_count = 0
+        for self.edge in edges:
+            self.stringToInt(self.edge)
+            if self.edge[START_NODE] >= self.vert_count or self.edge[END_NODE] >= self.vert_count:
+                if self.edge[START_NODE] > self.edge[END_NODE]:
+                    self.vert_count = self.edge[START_NODE]
+                else:
+                    self.vert_count = self.edge[END_NODE]
 
 
-    print(vert_count)
+        graph = {}
 
-    graph = {}
+        for self.edge in edges:
+            if self.edge[START_NODE] not in graph:
+                graph[self.edge[START_NODE]] = []
+            if self.edge[END_NODE] not in graph:
+                graph[self.edge[END_NODE]] = []
 
-    for edge in edges:
-        if edge[START_NODE] not in graph:
-            graph[edge[START_NODE]] = []
-        if edge[END_NODE] not in graph:
-            graph[edge[END_NODE]] = []
+            graph[self.edge[START_NODE]].append([self.edge[EDGE_ID],self.edge[END_NODE],self.edge[LENGTH]])
 
-        graph[edge[START_NODE]].append([edge[EDGE_ID],edge[END_NODE],edge[LENGTH]])
+        return graph
 
-    print(graph)
 
-    #return graph
-
-main()
-
+if __name__ == '__main__':
+    graph = Graph()
+    graph_data = graph.createGraph()
+    graph.printGraph(graph_data)
